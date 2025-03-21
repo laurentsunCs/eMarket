d6be0ca32210fef0145679d86a48fc46ef5e6313
Laurent Sun <laurentsun@MacBook-Pro-de-Laurent.local>
2025-03-21 16:36:11 +0100

    comment file directory

--- eMarket_1/src/java/model/Product.java
+++ eMarket_1/src/java/model/Product.java
@@ -1,6 +1,5 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/eMarket_1/src/java/model/Product.java
  */
 package model;
 
